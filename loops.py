4dcb61ab0ad69eb2b8a4bdf3039e6d03c5ab563e
Nandakumar9110 <sandulanandakumar@gmail.com>
2025-12-25 20:15:52 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,16 @@
+for i in range(10):#forloop
+    print(i)
+for i in range(1,10):
+    print(i)
+for i in range(0,20,5):
+    print(i)
+a=[1,2,3,4,5]
+for i in a:
+    print(i)
+b="pythonlife"
+for i in b:
+    print(i)
+nanda=30#whileloop
+while nanda<40:
+    print(nanda)
+    nanda+=1
