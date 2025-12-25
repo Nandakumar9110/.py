86ba1d8ca7a971282f531cdd9c105afd39a7efe0
Nandakumar9110 <sandulanandakumar@gmail.com>
2025-12-25 20:15:01 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,60 @@
+age=12 #if
+if age==12:
+    print("nanda")
+elif age >12:
+    print("kumar")
+else:
+    print("sandula")
+age=12 #else
+if age>12:
+    print("nanda")
+elif age >12:
+    print("kumar")
+else:
+    print("sandula")
+age=12 #if
+if age==12:
+    print("nanda")
+elif age ==12:
+    print("kumar")
+else:
+    print("sandula")
+age=12 #elif
+if age>12:
+    print("nanda")
+elif age ==12:
+    print("kumar")
+else:
+    print("sandula")
+e=50
+if e>40:#nested if
+    print("A")
+    if e<60:
+        print("b")
+    else:
+        print("c")
+else:
+    print("d")
+e=50
+if e>40:#nested if
+    print("A")
+    if e>60:
+        print("b")
+    else:
+        print("c")
+else:
+    print("d")
+e=50
+if e>50:#nested if
+    print("A")
+    if e<60:
+        print("b")
+    else:
+        print("c")
+else:
+    print("d")
+
+
+
+
+
