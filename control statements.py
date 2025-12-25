4f3e0d80542c1681570e0f6c3097e80aa7289770
Nandakumar9110 <sandulanandakumar@gmail.com>
2025-12-25 20:13:49 +0530

    Add files via upload

---
+++ control statements.py
@@ -0,0 +1,12 @@
+for i in range(10):#continue
+    if i==3:
+        continue
+    print(i)
+for i in range(10):#continue
+    if i==3:
+        break
+    print(i)
+for i in range(10):#continue
+    if i==20:
+        pass
+    print(i)
