af5bbe5780bc1734f06e12fe710e141abb344c7d
Nandakumar9110 <sandulanandakumar@gmail.com>
2025-12-25 20:09:35 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,13 @@
+a=11223344#int
+print(type(a))
+print(id(a))#memorylocation
+b=2.33#float
+print(type(b))
+c=1+2j#complex
+print(type(c))
+d=True#boolean
+e=False
+print(type(d),type(e))
+print(float(a))#explict type( no data loss)
+print(int(b))#implict type( data loss)
+
